7d4147fd9b922a8362eff5e9f15e1f1417bbbcd2
Ruaidhri McDonnell <ruaidhri.mcdonnell@gmail.com>
2018-03-09 14:32:35 +0000

    include missing file (woops)

---
+++ src/Tests/Eshopworld.Caching.Cosmos.Tests/LocalClusterCosmosDb.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Eshopworld.Caching.Cosmos.Tests
+{
+    internal class LocalClusterCosmosDb
+    {
+        public static Uri ConnectionURI { get; } = new Uri("https://localhost:8081");
+        public static string AccessKey { get; } = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        public static string DbName { get; } = "test-db";
+    }
+}
